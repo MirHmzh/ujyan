b7801c6b6aefb03a55a2674986d065685fc8be96
Amir Hamzah <corn.row81@gmail.com>
2016-06-22 14:35:54 +0700

    Minor changes HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -47,31 +47,31 @@ class HomeController extends Controller
 
     public function pdf()
     {
-    	return view('exam.result');
+        return view('exam.result');
     }
 
     public function printpdf()
     {
-    	$fork = Input::all();
+        $fork = Input::all();
         $target = Sentry::getUser()->id;
         $data = User::find($target);
         $data->finished = '1';
         $data->save();
 
-    	// instantiate and use the dompdf class
-		$dompdf = new Dompdf();
-		$html = View::make('exam.result', array('fork' => $fork, 'i' => 0));
-		$dompdf->loadHtml($html);
+        // instantiate and use the dompdf class
+        $dompdf = new Dompdf();
+        $html = View::make('exam.result', array('fork' => $fork, 'i' => 0));
+        $dompdf->loadHtml($html);
 
-		// (Optional) Setup the paper size and orientation
-		$dompdf->setPaper('A4', 'potrait');
+        // (Optional) Setup the paper size and orientation
+        $dompdf->setPaper('A4', 'potrait');
 
-		// Render the HTML as PDF
-		$dompdf->render();
-		$pdfname = Sentry::getUser()->email;
+        // Render the HTML as PDF
+        $dompdf->render();
+        $pdfname = Sentry::getUser()->email;
 
-		$output = $dompdf->output();
-		file_put_contents('result/'.$pdfname.'.pdf', $output);
+        $output = $dompdf->output();
+        file_put_contents('result/'.$pdfname.'.pdf', $output);
         Alert::success('Telah Mengerjakan Dengan Baik, Semoga Sukses!', 'Terima Kasih');
         return view('home');
     }
